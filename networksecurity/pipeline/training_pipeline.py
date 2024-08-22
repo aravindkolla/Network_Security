cbdcb68bdd9ddb2c6c18026b7c1f36c285456958
aravindkolla <aravindk5919@gmail.com>
2024-08-22 15:59:03 +0530

    data ingestion

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -11,14 +11,15 @@ from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.components.model_evaluation import ModelEvaluation
 from networksecurity.components.model_pusher import ModelPusher
 
-from networksecurity.entitiy.config_entity import(
-     TrainingPipelineConfig,
+from networksecurity.entity.config_entity import(
+    TrainingPipelineConfig,
     DataIngestionConfig,
     DataValidationConfig,
     DataTransformationConfig,
     ModelTrainerConfig,
     ModelEvaluationConfig,
     ModelPusherConfig
+   
 )
 
 from networksecurity.entity.artifact_entity import (
@@ -31,14 +32,20 @@ from networksecurity.entity.artifact_entity import (
 )
 
 
-
 class TrainingPipeline:
     def __init__(self):
-        pass
-
+         self.training_pipeline_config = TrainingPipelineConfig()
+    
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
+            
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -53,7 +60,7 @@ class TrainingPipeline:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
+    
     def start_model_trainer(self):
         try:
             pass
@@ -74,8 +81,9 @@ class TrainingPipeline:
         
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact=self.start_data_ingestion()
+            print(data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
     
-      
