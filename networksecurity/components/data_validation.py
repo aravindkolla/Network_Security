df681df878f239c020f2697f57f1e4dc335e9bc6
aravindkolla <41011538+aravindkolla@users.noreply.github.com>
2024-08-26 16:28:57 +0000

    utils file added

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -1,3 +1,56 @@
+from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.exception.exception import NetworkSecurityException 
+from networksecurity.logger.logger import logging 
+from networksecurity.utils.main_utils.utils import read_yaml_file,write_yaml_file
+from scipy.stats import ks_2samp
+import pandas as pd
+import os,sys
+
 class DataValidation:
-    def __init__(self):
-        pass
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact,
+                 data_validation_config: DataValidationConfig):
+        try:
+            self.data_ingestion_artifact=data_ingestion_artifact
+            self.data_validation_config=data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def validate_number_of_columns(self, dataframe:pd.DataFrame)->bool:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def is_numeric_column_exist(self, dataframe:pd.DataFrame)->bool:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    @staticmethod
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def detect_dataset_drift(self, base_df,current_df, threshold=0.05)->bool:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def initiate_data_validation(self)->DataValidationArtifact:
+        try:
+            self.read_data()
+            self.validate_number_of_columns()
+            self.is_numeric_column_exist()
+            self.detect_dataset_drift()
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+ 
