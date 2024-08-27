d264ec0cd391f7c72471dcb23144acddbd355310
aravindkolla <aravindk5919@gmail.com>
2024-08-27 11:30:21 +0000

    data transformation done

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -13,23 +13,35 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+    
 
 @dataclass
 class DataTransformationArtifact:
-    pass 
-
-@dataclass
-class ModelTrainerArtifact:
-    pass
-
-@dataclass
-class ModelEvaluationArtifact:
-    pass
-
-@dataclass
-class ModelPusherArtifact:
-    pass
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
 
 @dataclass
 class ClassificationMetricArtifact:
-    pass
+    f1_score: float
+    precision_score: float
+    recall_score: float
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted: bool
+    improved_accuracy: float
+    best_model_path: str
+    trained_model_path: str
+    train_model_metric_artifact: ClassificationMetricArtifact
+    best_model_metric_artifact: ClassificationMetricArtifact
+
+@dataclass
+class ModelPusherArtifact:
+    saved_model_path:str
+    model_file_path:str
